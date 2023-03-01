f391cfaf736139e8393464c98ad683cb914f3be0
Falase Femi <femifalase228@gmail.com>
2023-03-01 08:20:16 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Navbar } from './components'
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <>
+    <Navbar />
+    </>
   )
 }
 
