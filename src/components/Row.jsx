d5839ddaaa9b30d566e7442f442c9b0a3cc12de9
Falase Femi <femifalase228@gmail.com>
2023-03-01 23:58:55 +0100

    change document.getElementById to the react hook useRef

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Movie from "./Movie";
-import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
-  
+  const sliderRef = useRef(null);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -14,29 +14,38 @@ const Row = ({ title, fetchURL, rowID }) => {
   }, [fetchURL]);
 
   const sliderLeft = () => {
-    var slider = document.getElementById('slider' + rowID)
-    slider.scrollLeft = slider.scrollLeft - 500;
-  } 
+    sliderRef.current.scrollLeft -= 500;
+  };
 
   const sliderRight = () => {
-    var slider = document.getElementById('slider' + rowID)
-    slider.scrollLeft = slider.scrollLeft + 500;
-  } 
+    sliderRef.current.scrollLeft += 500;
+  };
 
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
-        <MdChevronLeft onClick={sliderLeft} className="bg-white left-0 rounded-full ansolute opacity-50 hover:opacity-100 cursor-pointer z-10 after:hidden group-hover:block" size={40} />
-        <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-normal scrollbar-hide relative">
+        <MdChevronLeft
+          onClick={sliderLeft}
+          className="bg-white left-0 rounded-full ansolute opacity-50 hover:opacity-100 cursor-pointer z-10 after:hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowID}
+          ref={sliderRef}
+          className="w-full h-full overflow-x-scroll whitespace-normal scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
-        <MdChevronRight onClick={sliderRight} className="bg-white right-0 rounded-full ansolute opacity-50 hover:opacity-100 cursor-pointer z-10 after:hidden group-hover:block" size={40} />
+        <MdChevronRight
+          onClick={sliderRight}
+          className="bg-white right-0 rounded-full ansolute opacity-50 hover:opacity-100 cursor-pointer z-10 after:hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
 };
-
-export default Row;
+ export default Row;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Main from "./Main";
-import Row from "./Row";
+import Row from './Row'
 import Movie from "./Movie";
 
 export {
